4b29f2e262802333678c55b9c5a652a851691f65
Vladyslav Antonchyk <109813312+BlaSSSTTT@users.noreply.github.com>
2024-10-24 17:25:09 +0300

    Correct modal

--- force-app/main/default/lwc/modalComponent/modalComponent.js
+++ force-app/main/default/lwc/modalComponent/modalComponent.js
@@ -7,44 +7,72 @@ import getModels from '@salesforce/apex/CarService.getModels';
 import getCars from '@salesforce/apex/CarService.getCars';
 
 export default class ModalComponent extends LightningModal {
-    @track selectedYear;
-    @track selectedBrand;
-    @track selectedModel;
-    @track selectedMake;
+    @track comboboxes =[
+        {
+            label:"Select Car Year",
+            placeholder:"Select Year",
+            value:null,
+            options:[],
+            onchange:this.handleYearChange,
+            isDisabled:false
+        },
+        {
+            label:"Select Car Brand",
+            placeholder:"Select Brand",
+            value:null,
+            options:[],
+            onchange:this.handleBrandChange,
+            isDisabled:true
+        },
+        {
+            label:"Select Car Model",
+            placeholder:"Select Model",
+            value:null,
+            options:[],
+            onchange:this.handleModelChange,
+            isDisabled:true
+        },
+        {
+            label:"Select Car",
+            placeholder:"Select Car",
+            value:null,
+            options:[],
+            onchange:this.handleMakeChange,
+            isDisabled:true
+        }
+    ]
 
-    @track yearOptions = [];
-    @track brandOptions = [];
-    @track modelOptions = [];
-    @track makeOptions = [];
 
-    @track disableBrand = true;
-    @track disableModel = true;
-    @track disableMake = true;
 
     connectedCallback() {
         this.loadYearOptions();
     }
 
     async loadYearOptions() {
+        
         try {
             const result = await getYears();
-            this.yearOptions = result.map(year => ({ label: year, value: year }));
+            this.comboboxes[0].options = result.map(year => ({ label: year, value: year }));
         } catch (error) {
             console.error('Error loading years: ', error);
             this.showToast('Error', 'Error loading years: ' + error.body.message, 'error'); 
         }
     }
 
-    handleYearChange(event) {
-        this.selectedYear = event.detail.value;
-        this.disableBrand = false; 
+    handleYearChange(event) {        
+        this.comboboxes[0].value = event.detail.value;
+        this.comboboxes[1].isDisabled = false; 
+        this.comboboxes[2].isDisabled = true;
+        this.comboboxes[3].isDisabled = true;
+        this.comboboxes[2].options = [];
+        this.comboboxes[3].options = [];
         this.loadBrandOptions(); 
     }
 
     async loadBrandOptions() {
         try {
-            const result = await getBrands({ year: this.selectedYear });
-            this.brandOptions = result.map(brand => ({ label: brand, value: brand }));
+            const result = await getBrands({ year: this.comboboxes[0].value });
+            this.comboboxes[1].options = result.map(brand => ({ label: brand, value: brand }));
         } catch (error) {
             console.error('Error loading brands: ', error);
             this.showToast('Error', 'Error loading brands: ' + error.body.message, 'error');
@@ -52,15 +80,17 @@ export default class ModalComponent extends LightningModal {
     }
 
     handleBrandChange(event) {
-        this.selectedBrand = event.detail.value;
-        this.disableModel = false; 
+        this.comboboxes[1].value = event.detail.value;
+        this.comboboxes[2].isDisabled = false; 
+        this.comboboxes[3].isDisabled = true;
+        this.comboboxes[3].options = [];
         this.loadModelOptions(); 
     }
 
     async loadModelOptions() {
         try {
-            const result = await getModels({ year: this.selectedYear, Brand: this.selectedBrand });
-            this.modelOptions = result.map(model => ({ label: model, value: model }));
+            const result = await getModels({ year: this.comboboxes[0].value, Brand: this.comboboxes[1].value });
+            this.comboboxes[2].options = result.map(model => ({ label: model, value: model }));
         } catch (error) {
             console.error('Error loading models: ', error);
             this.showToast('Error', 'Error loading models: ' + error.body.message, 'error'); 
@@ -68,15 +98,15 @@ export default class ModalComponent extends LightningModal {
     }
 
     handleModelChange(event) {
-        this.selectedModel = event.detail.value;
-        this.disableMake = false; 
+        this.comboboxes[2].value = event.detail.value;
+        this.comboboxes[3].isDisabled = false; 
         this.loadMakeOptions(); 
     }
 
     async loadMakeOptions() {
         try {
-            const result = await getCars({ year: this.selectedYear, Brand: this.selectedBrand, Model: this.selectedModel });
-            this.makeOptions = result.map(make => ({ label: make, value: make }));
+            const result = await getCars({ year: this.comboboxes[0].value, Brand: this.comboboxes[1].value, Model: this.comboboxes[2].value });
+            this.comboboxes[3].options = result.map(make => ({ label: make, value: make }));
         } catch (error) {
             console.error('Error loading makes: ', error);
             this.showToast('Error', 'Error loading makes: ' + error.body.message, 'error'); 
@@ -89,10 +119,10 @@ export default class ModalComponent extends LightningModal {
 
     handleSubmit() {
         this.close({
-            year: this.selectedYear,
-            brand: this.selectedBrand,
-            model: this.selectedModel,
-            name: this.selectedMake
+            year:  this.comboboxes[0].value,
+            brand: this.comboboxes[1].value,
+            model: this.comboboxes[2].value,
+            name: this.comboboxes[3].value
         });
     }
 
